344a1e9104024a4eb7a553c6d3349de5fddf3dc5
osiris <wendyosi25@gmail.com>
2021-03-25 20:57:50 -0600

    manda el status del url

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 const fs = require('fs');
 const path = require('path');
-const markdownIt = require('markdown-it');
-
+const fetch = require('node-fetch');
+const { ok } = require('assert');
 /*FUNCTION READ FILES */
-const readArchives = (archive) =>{
-	fs.readFile(archive, 'utf8', (error,archiveData)=>{
-
-			if(error){
+const readArchives = (archive) => {
+	fs.readFile(archive, 'utf8', (error, archiveData) => {
+		if (error) {
 			console.log(`Error function readArchive ${error}`);
-		}else{
-				getLinks(archiveData);
+		} else {
+			getLinks(archiveData);
 		}
 	});
-
 }
 
-const readDirectiry = (archive) =>{
-	fs.readdir(archive,(error,files)=>{
-		if(error){
-		}else{
-			files.forEach((doc)=>{
+const readDirectiry = (archive) => {
+	fs.readdir(archive, (error, files) => {
+		if (error) {
+		} else {
+			files.forEach((doc) => {
 				const extNamePath = path.extname(doc);
 				const newPath = archive + '/' + doc;
-				if (extNamePath == '.md') {
-			 	readArchives(newPath);
-				}else if ( extNamePath == '' ){
+				console.log("Path",newPath);
+				if (extNamePath === '.md') {
+					readArchives(newPath);
+				} else if (extNamePath === '') {
 					readDirectiry(newPath);
 				}
 			});
@@ -34,18 +33,44 @@ const readDirectiry = (archive) =>{
 }
 readDirectiry('./Directorio1');
 
-const getLinks = (archives) =>{
+const getLinks = (archives) => {
+	const text = archives.split('\n');
+	text.forEach(element => {
+		const linksHttp = /http/.test(element);
+		const linksHttps = /https/.test(element);
+		if (linksHttp == true || linksHttps == true) {
+			const urlExpR = /(htt?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+			const urlExtracted = element.match(urlExpR);
+			if (urlExtracted) {
+				urlExtracted.forEach(url => {
+					depuringUrl(url);
+				})
+			}
+		}
+	});
+}
+//
+const depuringUrl= (url)=>{
+	const cleaningLinks = url.indexOf(')');
+	let onlyLinks;
+	if (cleaningLinks !== -1) {
+		onlyLinks = url.slice(0, cleaningLinks);
+		//console.log( onlyLinks);
+	} else {
+		onlyLinks = url;
+		//console.log(onlyLinks);
+	}
+	peticionFetch(onlyLinks);
+}
 
-			const text = archives.split('\n');
-			text.forEach(element => {
-				const linksHttp=/http/.test(element);
-				const linksHttps = /https/.test(element);
-				if (linksHttp === true || linksHttps=== true){
-
-				 console.log("Http",linksHttp, element);
-					console.log("Https", linksHttps, element);
-				}
-
-
-			});
+const peticionFetch = (url)=>{
+	 fetch(url)
+ .then(response =>{
+	 if(response){
+		 console.log(url,response.status);
+	 }else{
+		 console.log(url,response.status);
+	 }
+ })
+ .catch(error => console.log('ERROR',error));
 }
